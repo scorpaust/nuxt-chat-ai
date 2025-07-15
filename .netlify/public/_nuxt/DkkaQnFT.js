908fa007df1491b306d36bfea067ed4bb8dca1a3
scorpaust <scorpaust@hotmail.com>
2025-07-15 23:49:50 +0100

    fix(netlify): embed prisma client

--- .netlify/public/_nuxt/DkkaQnFT.js
+++
@@ -1,1 +0,0 @@
-import{d as a,n as o}from"./C3KKrbky.js";import{u as i}from"./CKJQ9wQT.js";const s=a((t,r)=>{const{isAuthenticated:e}=i();if(t.path!=="/login"&&!e.value)return o("/login")});export{s as default};
