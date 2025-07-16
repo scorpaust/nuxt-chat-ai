7562a71f06d38daa121eec9ccf53321c9a0d1656
scorpaust <scorpaust@hotmail.com>
2025-07-16 01:54:17 +0100

    fix(netlify): embed prisma client

--- layers/auth/app/middleware/auth.ts
+++ layers/auth/app/middleware/auth.ts
@@ -1,11 +1,6 @@
 // middleware/auth.global.ts
-export default defineNuxtRouteMiddleware((to, _from) => {
+export default defineNuxtRouteMiddleware((_to, _from) => {
   const { isAuthenticated } = useAuth();
-
-  // 1) se for rota de login, deixa passar
-  if (to.path === "/login") {
-    return;
-  }
 
   // 2) se não estiver autenticado, redireciona para /login
   if (!isAuthenticated.value) {
