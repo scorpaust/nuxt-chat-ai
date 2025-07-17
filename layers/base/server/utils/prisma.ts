4db9cd67c9c774484e1612f24ea4b7f1f948d802
scorpaust <scorpaust@hotmail.com>
2025-07-17 22:25:54 +0100

    fix(netlify): embed prisma client

--- layers/base/server/utils/prisma.ts
+++ layers/base/server/utils/prisma.ts
@@ -1,22 +1,10 @@
 // server/db.ts
-import { createRequire } from "module";
-const require = createRequire(import.meta.url);
+import { PrismaClient } from "@prisma/client";
 
 // hold onto a single client instance
-let prisma: import("@prisma/client").PrismaClient | null = import(
-  "@prisma/client"
-).PrismaClient;
+const prisma = PrismaClient;
 
 export function getPrismaClient() {
   // if we're in a prerender build, bail out
-  if (import.meta.env.NITRO_PRENDERING) {
-    throw new Error("PrismaClient is not available during prerender");
-  }
-
-  if (!prisma) {
-    // use CommonJS require to avoid ESM name‑export issues
-    const pkg = require("@prisma/client") as typeof import("@prisma/client");
-    prisma = new pkg.PrismaClient();
-  }
   return prisma;
 }
